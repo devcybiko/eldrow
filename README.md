739aa0a7b63ef3dddfbbb3003c2a18d328fbcf0f
Greg Smith <thinkable@thinkable.us>
2022-01-19 18:52:18 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# eldrow
+# ELDROW - The Wordle Solver
+
+## Command
+`eldrow guess1=pattern guess2=pattern...`
+
+*  with no parameters, it will generate a first guess
+*  with parameters, it will generate a next guess
+*  enter guesses and results from Wordle as follows
+   - pattern has a letter for each letter in the guess
+   - G - for green
+   - Y - for yellow
+   - B - for black
+  
+## Example:
+```
+  $ eldrow.js
+  CARES
+  $ eldrow.js cares=BBBBY
+  SOILY (-but the game doesn't know SOILY! -)
+  $ eldrow.js cares=BBBBY --omit=SOILY
+  SONLY
+  $ eldrow.js cares=BBBBY SONLY=GBYBB
+  SUINT
+  $ eldrow.js cares=BBBBY SONLY=GBYBB
+  SPUNK
+  $ eldrow.js cares=BBBBY SONLY=GBYBB SPUNK=GBGGB
+  SHUNS (- winning word -)
+```
--- eldrow.js
+++ eldrow.js
@@ -6,14 +6,138 @@
 
 const glstools = require("glstools");
 const gprocs = glstools.procs;
-const gstrings = glstools.strings;
-const gfiles = glstools.files;
-const fs = require("fs");
-const path = require("path");
+const gmaths = glstools.maths;
 const { die } = require("glstools/js/glsprocs");
+const { Dictionary } = require("./Dictionary");
+const { Histogram } = require("./Histogram");
+
+async function main$(_opts) {
+  let opts = getOpts(_opts);
+  let pastGuesses = opts._files;
+  let dictionary = new Dictionary(opts.wordfile, opts.omitfile);
+  if (opts.omit) {
+    dictionary.omitWords([opts.omit]);
+    dictionary.saveOmittedWords();
+  }
+
+  let histogram = new Histogram(dictionary.getWords());
+  dictionary.scoreWords(histogram);
+
+  printPastGuesses(pastGuesses, dictionary);
+
+  let patterns = computePatterns(pastGuesses);
+  let guesses = computeGuesses(dictionary, patterns, pastGuesses.length > 3);
+  guesses = guessAtLettersNotUncovered(dictionary, patterns, guesses, pastGuesses);
+  if (guesses.length === 0) guesses = computeGuesses(dictionary, patterns, true);
+  if (guesses.length === 0) die("I'M STUPMPED!");
+  let bestGuesses = guesses.filter((guess) => guess.score === guesses[0].score);
+  console.log(bestGuesses);
+  console.log(bestGuesses[gmaths.random(0, bestGuesses.length - 1)].word);
+}
+
+module.exports = { main$ };
+
+if (module.id === ".") {
+  return main$();
+}
+
+function computePatterns(guesses) {
+  let patterns = [];
+  let must = {};
+  let mustnot = {};
+  for (let pos of range(0, 5)) patterns[pos] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let guess of guesses) {
+    let parts = guess.toUpperCase().split("=");
+    let word = parts[0];
+    let answer = parts[1];
+    for (let pos of range(0, 5)) {
+      if (answer[pos] === "G") {
+        patterns[pos] = word[pos];
+        must[word[pos]] = word[pos];
+      }
+      if (answer[pos] === "B") {
+        for (let i in range(0, 5))
+          patterns[i] = remove(patterns[i], word[pos]);
+        mustnot[word[pos]] = word[pos];
+      }
+      if (answer[pos] === "Y") {
+        patterns[pos] = remove(patterns[pos], word[pos]);
+        must[word[pos]] = word[pos];
+      }
+    }
+  }
+  patterns.must = Object.keys(must);
+  patterns.mustnot = Object.keys(mustnot);
+  return patterns;
+}
+
+function computeGuesses(dictionary, patterns, allowDups = true) {
+  let list = [];
+  let score = 1e99;
+  for (let entry of dictionary.getScores()) {
+    let word = entry.word;
+    let uniqueLetters = {};
+    let pos;
+    for (pos of range(0, 5)) {
+      if (!patterns[pos].includes(word[pos])) break;
+      uniqueLetters[word[pos]] = word[pos];
+    }
+    if (pos === 4) {
+      if (!includesAll(word, patterns.must)) continue;
+      if (Object.keys(uniqueLetters).length === 5 || allowDups) {
+        list.push(entry);
+        score = entry.score;
+      }
+    }
+  }
+  return list;
+}
+
+function guessAtLettersNotUncovered(dictionary, patterns, guesses, pastGuesses) {
+  let alternatives = guesses;
+  if (patterns.must.length >= 3 && guesses.length > 6 - pastGuesses.length) {
+    console.log("TRYING TO UNMASK OTHER LETTERS");
+    let newPatterns = [];
+    newPatterns.must = [];
+    for (let i of range(0, patterns.length)) {
+      newPatterns[i] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      for (let c of patterns.must)
+        newPatterns[i] = remove(newPatterns[i], c);
+      for (let c of patterns.mustnot)
+        newPatterns[i] = remove(newPatterns[i], c);
+    }
+    alternatives = computeGuesses(dictionary, newPatterns, false);
+  }
+  return alternatives;
+}
+
+function getOpts(_opts) {
+  let opts =
+    _opts ||
+    gprocs.args(
+      "--help,--omitfile=omit.txt,--omit=,--wordfile=five-letter-words.txt",
+      "word1,word2,word3,word4,word5,word6"
+    );
+  if (opts.help) return help();
+  for(let i of range(0, opts._files.length)) {
+    let opt = opts._files[i];
+    opts._files[i] = opt.toUpperCase();
+    let parts = opts._files[i].split("=");
+    if (
+      parts.length !== 2 ||
+      parts[0].length !== 5 ||
+      parts[1].length !== 5 ||
+      !parts[0].match("[A-Z]{5}") ||
+      !parts[1].match("[BGY]{5}")
+      ) die(`  Option '${opt}': invalid format`)
+  }
+  return opts;
+}
 
 function help() {
-  console.log("\neldrow guess1=pattern guess2=pattern...");
+  console.log("\n");
+  console.log("ELDROW - The Wordle Solver\n");
+  console.log("eldrow guess1=pattern guess2=pattern...\n");
   console.log("  with no parameters, it will generate a first guess");
   console.log("  with parameters, it will generate a next guess");
   console.log("  enter guesses and results from Wordle as follows");
@@ -21,116 +145,28 @@ function help() {
   console.log("  - G - for green");
   console.log("  - Y - for yellow");
   console.log("  - B - for black");
-  console.log("  example:");
-  console.log("  $ eldrow");
-  console.log("  > SHARE");
-  console.log("  $ eldrow share=GBBYB");
-  console.log("  > SORTE");
-  console.log("  $ eldrow share=GBBYB sorte=GBBYB");
+  console.log("example:");
+  console.log("  $ eldrow.js");
+  console.log("  CARES");
+  console.log("  $ eldrow.js cares=BBBBY");
+  console.log("  SOILY (-but the game doesn't know SOILY! -)");
+  console.log("  $ eldrow.js cares=BBBBY --omit=SOILY");
+  console.log("  SONLY");
+  console.log("  $ eldrow.js cares=BBBBY SONLY=GBYBB");
+  console.log("  SUINT");
+  console.log("  $ eldrow.js cares=BBBBY SONLY=GBYBB");
+  console.log("  SPUNK");
+  console.log("  $ eldrow.js cares=BBBBY SONLY=GBYBB SPUNK=GBGGB");
+  console.log("  SHUNS (- winning word -)");
   gprocs.die("");
 }
 
-function computeHistogram_save(words) {
-    // compute frequency of occurance of letters
-    let histogram = [];
-    for (let pos = 0; pos < 5; pos++) {
-      histogram[pos] = [];
-    }
-    for (let word of words) {
-      for (let pos = 0; pos < 5; pos++) {
-        word = word.toUpperCase();
-        if (word.length !== 5) continue;
-        let char = word[pos];
-        let n = char.charCodeAt(0) - "A".charCodeAt(0);
-        let elem = histogram[pos][n] || { key: char, count: 0 };
-        elem.count++;
-        histogram[pos][n] = elem;
-      }
-    }
-    histogram.map((list) => list.sort((a, b) => b.count - a.count));
-    histogram.map((list) =>
-      list.map((elem, index) => {
-        elem.index = index;
-        return elem;
-      })
-    );
-    console.log(histogram);
-    return histogram;
+function range(min, max) {
+  let r = [];
+  for (let i = min; i < max; i++) {
+    r.push(i);
   }
-
-  function computeHistogram(words) {
-    // compute frequency of occurance of letters
-    let histogram = [];
-    for (let pos = 0; pos < 5; pos++) {
-      histogram[pos] = [];
-    }
-    for (let word of words) {
-      for (let pos = 0; pos < 5; pos++) {
-        word = word.toUpperCase();
-        if (word.length !== 5) continue;
-        let char = word[pos];
-        let n = char.charCodeAt(0) - "A".charCodeAt(0);
-        let elem = histogram[0][n] || { key: char, count: 0 };
-        elem.count++;
-        histogram[0][n] = elem;
-      }
-    }
-    histogram[1] = histogram[0];
-    histogram[2] = histogram[0];
-    histogram[3] = histogram[0];
-    histogram[4] = histogram[0];
-    histogram.map((list) => list.sort((a, b) => b.count - a.count));
-    histogram.map((list) =>
-      list.map((elem, index) => {
-        elem.index = index;
-        return elem;
-      })
-    );
-    console.log(histogram);
-    return histogram;
-  }
-
-
-function computeDictionary(omits, wordfile, dictionaryfile) {
-    let dictionary = gfiles.readList(dictionaryfile);
-    if (dictionary) {
-        for(let omit of omits) {
-            let n = dictionary.indexOf(omit);
-            if (n > -1) dictionary.splice(n, 1);
-        }
-        return dictionary;
-    }
-
-    let words = gfiles
-    .readList(wordfile)
-    .map((line) => line.trim());
-    let histogram = computeHistogram(words);
-
-
-  let lookup = {};
-  for (let word of words) {
-    lookup[word] = word;
-  }
-  dictionary = [];
-  for (let i0 = 0; i0 < 26; i0++) {
-    for (let i1 = 0; i1 < 26; i1++) {
-      for (let i2 = 0; i2 < 26; i2++) {
-        for (let i3 = 0; i3 < 26; i3++) {
-          for (let i4 = 0; i4 < 26; i4++) {
-            let word =
-              histogram[0][i0].key +
-              histogram[1][i1].key +
-              histogram[2][i2].key +
-              histogram[3][i3].key +
-              histogram[4][i4].key;
-            if (lookup[word] && !omits.includes(word)) dictionary.push(word);
-          }
-        }
-      }
-    }
-  }
-  gfiles.writeList(dictionaryfile, dictionary);
-  return dictionary;
+  return r;
 }
 
 function remove(s, c) {
@@ -142,36 +178,6 @@ function remove(s, c) {
   return a;
 }
 
-function computePossibles(guesses) {
-  let possibles = [];
-  let must = {};
-  for (let pos = 0; pos < 5; pos++) {
-    possibles[pos] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  }
-  for (let guess of guesses) {
-    let parts = guess.toUpperCase().split("=");
-    let word = parts[0];
-    let answer = parts[1];
-    for (let pos = 0; pos < 5; pos++) {
-      if (answer[pos] === "G") {
-        possibles[pos] = word[pos];
-        must[word[pos]] = word[pos];
-      }
-      if (answer[pos] === "B") {
-        for (let i = 0; i < 5; i++)
-          possibles[i] = remove(possibles[i], word[pos]);
-      }
-      if (answer[pos] === "Y") {
-        possibles[pos] = remove(possibles[pos], word[pos]);
-        must[word[pos]] = word[pos];
-      }
-    }
-  }
-  possibles.must = Object.keys(must);
-  console.log(possibles);
-  return possibles;
-}
-
 function includesAll(word, must) {
   for (let m of must) {
     if (!word.includes(m)) return false;
@@ -179,61 +185,12 @@ function includesAll(word, must) {
   return true;
 }
 
-function computeGuess(dictionary, possibles, nodups = false) {
-  for (let word of dictionary) {
-    let pos;
-    let group = {};
-    for (pos = 0; pos < 5; pos++) {
-      if (!possibles[pos].includes(word[pos])) break;
-      group[word[pos]] = word[pos];
-    }
-    if (pos === 5) {
-      if (!includesAll(word, possibles.must)) continue;
-      if (!nodups) return word;
-      if (Object.keys(group).length === 5) return word;
-    }
-  }
-  return null;
-}
-
-function computeGuess_save(dictionary, possibles) {
-  let stack = [0, 0, 0, 0, 0];
-  let guess = "";
-  while (true) {
-    guess = "";
-    for (let pos = 0; pos < 5; pos++) guess += possibles[pos][stack[pos]];
-    if (dictionary.includes(guess)) return guess;
-    for (let i = 0; i < 5; i++) {
-      stack[i] += 1;
-      if (stack[i] < possibles[i].length) break;
-      stack[i] = 0;
-      if (i === 4) break;
-    }
-  }
-  return guess;
-}
-async function main$(_opts) {
-  let opts =
-    _opts ||
-    gprocs.args(
-      "--help,--omitfile=omit.txt,--omit=,--wordfile=five-letter-words.txt,--dictionary=dictionary.txt",
-      "word1,word2,word3,word4,word5,word6"
+function printPastGuesses(pastGuesses, dictionary) {
+  for (let guess of pastGuesses)
+    console.log(
+      guess,
+      dictionary
+        .getScores()
+        .filter((dict) => dict.word === guess.substring(0, 5))
     );
-  if (opts.help) help();
-  let omits = gfiles.readList(opts.omitfile);
-  if (opts.omit) {
-      omits.push(opts.omit);
-      gfiles.writeList(opts.omitfile, omits);
-  }
-  let dictionary = computeDictionary(omits, opts.wordfile, opts.dictionary);
-  let guesses = opts._files;
-  let possibles = computePossibles(guesses);
-  let guess = computeGuess(dictionary, possibles, guesses.length === 0);
-  console.log(guess);
-}
-
-module.exports = { main$ };
-
-if (module.id === ".") {
-  return main$();
 }
